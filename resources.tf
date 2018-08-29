97009ad31180218189467c251105b41cfc004f89
Antonije Tadic <aco@promonte.com>
2018-08-29 14:43:15 +0200

    cnahges: security group

--- resources.tf
+++ resources.tf
@@ -12,3 +12,22 @@ resource "aws_subnet" "subnet1" {
     vpc_id = "${aws_vpc.enviroment-example-two.id}"
     availability_zone = "eu-central-1a"
 }
+
+resource "aws_subnet" "subnet2" {
+    cidr_block = "${cidrsubnet(aws_vpc.enviroment-example-two.cidr_block,2,2)}"
+    vpc_id = "${aws_vpc.enviroment-example-two.id}"
+    availability_zone = "eu-central-1b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = "${aws_vpc.environment-example-two.id}"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = [
+          "${aws_vpc.environment-example-two.cidr_block}"
+      ]
+    }
+
+}
